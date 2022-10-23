acc3765f6d11b301d8a76b883e94b37958cd18d9
tahasafdari <41860385+tahasafdari@users.noreply.github.com>
2022-10-23 22:24:51 +0300

    javadoc
    javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"eduni.distributions"},{"l":"simu.framework"},{"l":"simu.model"},{"l":"test"},{"l":"testi"},{"l":"view"}]
